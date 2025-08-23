b319ae7311b2c15877d6f4806c1648a2e62cd870
pargame (auto) <pargame@example.com>
2025-08-24 02:28:48 +0900

    test: fix setupTests import for jest-dom matchers

--- test/setupTests.js
+++ test/setupTests.js
@@ -1,5 +1,5 @@
 import { expect } from 'vitest';
-import matchers from '@testing-library/jest-dom/matchers';
+import * as matchers from '@testing-library/jest-dom/matchers';
 
 // Extend vitest's expect with jest-dom matchers
 expect.extend(matchers);
